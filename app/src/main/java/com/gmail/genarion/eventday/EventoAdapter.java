54b4d43f18ca931200974d0a38c0cbd0ca2cb625
genarion <genarion@gmail.com>
2017-01-25 23:42:51 +0100

    lista acontecimientos

---
+++ app/src/main/java/com/gmail/genarion/eventday/EventoAdapter.java
@@ -0,0 +1,33 @@
+package com.gmail.genarion.eventday;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Mario on 06/02/2017.
+ */
+
+public class EventoAdapter  extends ArrayAdapter<EventoItem> {
+    private final ArrayList<EventoItem> items;
+    private final Context context;
+
+    public EventoAdapter(Context context, int textViewResourceId,ArrayList<EventoItem> items) {
+        super(context,textViewResourceId,items);
+        this.items = items;
+        this.context=context;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.listado_eventos, parent, false);
+        TextView textView = (TextView) view.findViewById(R.id.textView_nombre_evento);
+        textView.setText(items.get(position).getNombre());
+        return view;
+    }
+}
